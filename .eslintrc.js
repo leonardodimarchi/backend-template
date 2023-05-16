e297c8e2fff0c6f9670366306fb277791cd6cee7
User <leonardodimarchi@outlook.com>
2023-05-15 21:45:05 -0300

    chore(prettier && eslint && jest): adjusting prettier, eslint and jest configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,10 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 };
