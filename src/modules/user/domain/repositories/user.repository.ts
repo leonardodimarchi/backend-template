61f4fd348b006ff18ccf7cfdb30410e519267678
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-05-20 11:41:37 -0300

    fix(user): fixing an import

--- src/modules/user/domain/repositories/user.repository.ts
+++ src/modules/user/domain/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-import { UserEntity } from "../entities/user.entity";
+import { UserEntity } from "../entities/user/user.entity";
 
 export abstract class UserRepository {
     abstract save(user: UserEntity): Promise<void>;
