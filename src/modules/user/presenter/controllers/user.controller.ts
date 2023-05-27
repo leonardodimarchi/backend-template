e99544ed72c63af8a14c4e8f50c6f8e7fe6f1bd2
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-05-27 11:34:23 -0300

    fix(user): changing http response from 200 to 201

--- src/modules/user/presenter/controllers/user.controller.ts
+++ src/modules/user/presenter/controllers/user.controller.ts
@@ -1,8 +1,8 @@
 import { CreateUserUseCase } from '@modules/user/domain/usecases/create-user.usecase';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
 import { UserViewModel } from '../models/view-models/user.view-model';
 import { CreateUserPayload } from '../models/payloads/create-user.payload';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('users')
@@ -10,14 +10,10 @@ export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @ApiOperation({ summary: 'Creates a new user' })
-  @ApiOkResponse({ type: UserViewModel })
+  @ApiResponse({ status: HttpStatus.CREATED, type: UserViewModel })
   @Post()
   async create(@Body() payload: CreateUserPayload): Promise<UserViewModel> {
-    const {
-      email,
-      name,
-      password,
-    } = payload;
+    const { email, name, password } = payload;
 
     const { createdUser } = await this.createUserUseCase.exec({
       email,
