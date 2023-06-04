c89abe17db46fdf372b0d0065a7a0f31053fdecc
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-06-04 11:56:28 -0300

    feature: adding i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@shared/infra/database/database.module';
+import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     DatabaseModule,
     UserModule,
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        AcceptLanguageResolver,
+        QueryResolver,
+      ]
+    }),
   ],
   controllers: [],
   providers: [],
--- src/modules/user/presenter/controllers/user.controller.ts
+++ src/modules/user/presenter/controllers/user.controller.ts
@@ -10,10 +10,11 @@ import {
 } from '@nestjs/common';
 import { UserViewModel } from '../models/view-models/user.view-model';
 import { CreateUserPayload } from '../models/payloads/create-user.payload';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DuplicatedEmailError } from '@modules/user/domain/errors/duplicated-email.error';
 import { InvalidEmailError } from '@modules/user/domain/errors/invalid-email.error';
 import { InvalidNameError } from '@modules/user/domain/errors/invalid-name.error';
+import { I18nContext } from 'nestjs-i18n';
 
 @ApiTags('Users')
 @Controller('users')
@@ -21,6 +22,7 @@ export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @ApiOperation({ summary: 'Creates a new user' })
+  @ApiHeader({ name: 'Accept-Language', example: 'en', required: true })
   @ApiResponse({ status: HttpStatus.CREATED, type: UserViewModel })
   @Post()
   async create(@Body() payload: CreateUserPayload): Promise<UserViewModel> {
@@ -37,19 +39,22 @@ export class UserController {
     }
 
     if (result.value instanceof DuplicatedEmailError) {
-      throw new ConflictException('The provided e-mail already exists.', {
-        cause: result.value,
-      });
+      throw new ConflictException(
+        I18nContext.current()?.t('users.errors.duplicated-email'),
+        {
+          cause: result.value,
+        }
+      );
     }
 
     if (result.value instanceof InvalidEmailError) {
-      throw new BadRequestException('The provided e-mail is invalid.', {
+      throw new BadRequestException(I18nContext.current()?.t('users.errors.invalid-email'), {
         cause: result.value,
       });
     }
 
     if (result.value instanceof InvalidNameError) {
-      throw new BadRequestException('The provided name is invalid.', {
+      throw new BadRequestException(I18nContext.current()?.t('users.errors.invalid-name'), {
         cause: result.value,
       });
     }
