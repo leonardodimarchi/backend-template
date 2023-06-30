ec74d169a4a0af952526616011e2e0fcf3b40d94
leonardo dimarchi <leonardodimarchi@outlook.com>
2023-06-30 18:01:21 -0300

    feat: adding base-entity view model

---
+++ src/shared/presenter/models/base-entity.view-model.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { BaseEntity } from "@shared/domain/base.entity";
+import { UUID } from "crypto";
+
+export class BaseEntityViewModel {
+  constructor(entity: BaseEntity<any>) {
+    this.id = entity.id;
+    this.createdAt = entity.createdAt;
+    this.updatedAt = entity.updatedAt;
+  }
+
+  @ApiProperty({ example: '<UUID>' })
+  id: UUID;
+
+  @ApiProperty({ example: new Date() })
+  createdAt: Date;
+
+  @ApiProperty({ example: new Date() })
+  updatedAt: Date;
+}
